c152c13ab3f53f782a184368f77a7ef5163eb43f
Om Prakash S <prakash1996@gmail.com>
2017-04-01 01:59:21 +0530

    Added Timestamp naming feature to extracted Video Frames

--- lib/FileProcessor.py
+++ lib/FileProcessor.py
@@ -2,14 +2,25 @@ import glob, os
 from pprint import pprint
 from collections import Counter
 import operator 
+import re
 
+def sorted_nicely( l ):
+    """ Sorts the given iterable in the way that is expected.
+ 
+    Required arguments:
+    l -- The iterable to be sorted.
+ 
+    """
+    convert = lambda text: int(text) if text.isdigit() else text
+    alphanum_key = lambda key: [convert(c) for c in re.split('([0-9]+)', key)]
+    return sorted(l, key = alphanum_key)
 
 def FileRename(media_dir,video_id,timestamps):
 	print "INFO: Reading Directory %s for %s.mp4 files" % (media_dir,video_id)
 	files = glob.glob(media_dir+video_id+'.mp4*.jpg')
-
+        files = sorted_nicely(files)
 	counter=1
-	for oldname, ts in zip(sorted(files),timestamps):
+	for oldname, ts in zip(files,timestamps):
 		splitted_name = oldname.split(".mp4")
 		newname=splitted_name[0]+'_'+str(ts)+'_'+str(counter)+'.jpg'
 		print "%d : Changing %s to %s \n" %(counter,oldname,newname)
